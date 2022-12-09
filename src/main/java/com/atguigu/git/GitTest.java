efecf80ebbfd99098d6210a3d7805a996f5db1ae
ojqstart <ojqstart@163.com>
2022-12-09 22:58:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author ojqstart Email:ojqstart@163.com
+ * @Description
+ * @date 2022/12/9-22:40
+ */
+public class GitTest {
+	public static void main(String[] args){
+		System.out.println("hello git!");
+	}
+}
